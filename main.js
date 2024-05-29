fda769ed9f29647edfd3e12d16e299c60257d1c9
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2024-05-29 15:00:01 +0200

    resize and update camera events, improved lights

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import "./style.css"
 
 const scene = new THREE.Scene()
 
@@ -6,13 +7,19 @@ const scene = new THREE.Scene()
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  metalness: 0.2,
 })
 const mesh = new THREE.Mesh(geometry, material)
 
-const light = new THREE.PointLight(0xffffff, 2, 60)
-light.position.set(0, 10, 10)
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
 
-const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+const light = new THREE.PointLight("white", 1, 100)
+light.position.set(0, 20, 20)
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height)
 camera.position.z = 20
 
 scene.add(mesh, camera, light)
@@ -20,5 +27,23 @@ scene.add(mesh, camera, light)
 //Renderer
 const canvas = document.querySelector(".webgl")
 const renderer = new THREE.WebGLRenderer({ canvas })
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+//resize
+
+window.addEventListener("resize", () => {
+  //update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  //update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = () => {
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+}
+loop()
